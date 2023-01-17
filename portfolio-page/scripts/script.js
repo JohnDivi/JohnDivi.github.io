4ef7d497fbfe72310b41414e8ff82c8fa22d7e5b
JohnDivi <johnbdivinagracia23@gmail.com>
2023-01-16 17:12:28 -0700

    Deleted files not needed

--- portfolio-page/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function remove() {
-    var elem = document.getElementById("welcome");
-    elem.className = "remove-welcome";
-    setTimeout(function() {
-        elem.remove();
-    }, 750);
-    
-    return false;
-}
